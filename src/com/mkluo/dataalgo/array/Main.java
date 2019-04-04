557f5aa0e9654b0aa01a703a21701afcaf69a2d8
luomk <mkluo@g2link.cn>
2019-04-04 16:52:38 +0800

    数据结构与算法

---
+++ src/com/mkluo/dataalgo/array/Main.java
@@ -0,0 +1,24 @@
+package com.mkluo.dataalgo.array;
+
+/**
+ * @Description: TODO
+ * @Author: luomingkui
+ * @Date: Created in 下午4:37 2019/4/4
+ * @Version: V1.0 数组练习
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1,3,5,7,9,10};
+
+        for (int i=0;i<=arr.length;i++){
+            System.out.println(i);
+       }
+
+
+    }
+
+
+}
---
+++ src/com/mkluo/dataalgo/test/Test.java
@@ -0,0 +1,27 @@
+package com.mkluo.dataalgo.test;
+
+/**
+ * @Auther: luomingkui
+ * @Date: 2019/3/5 16:22
+ * @Description:
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        cal(5);
+
+
+
+    }
+
+    static int cal(int n) {
+        int sum =0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
+}
