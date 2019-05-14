65d8a5ac6e1a32fd9db0d85769b337817746531c
luomk <mkluo@g2link.cn>
2019-05-14 16:04:17 +0800

    Leetcode7

---
+++ src/com/mkluo/dataalgo/leetcode/LeetCode7.java
@@ -0,0 +1,32 @@
+package com.mkluo.dataalgo.leetcode;
+
+/**
+ * @Description: LeetCode-7-整数反转
+ * @Author: luomingkui
+ * @Date: Created in 下午3:36 2019/5/14
+ * @Version: V1.0
+ */
+
+public class LeetCode7 {
+
+    public static void main(String[] args) {
+        LeetCode7 leetCode7 = new LeetCode7();
+        int abc = leetCode7.reverse(123456);
+        System.out.println(abc);
+    }
+    public int reverse(int x) {
+        int r = 0;
+        if (x>=Integer.MAX_VALUE || x<=Integer.MIN_VALUE){
+            return 0;
+        }
+        while(x!=0){
+            r=10*r+x%10;
+            x=x/10;
+            if(r>Integer.MAX_VALUE/10&&x>0||r<Integer.MIN_VALUE/10&&x<0){
+                return 0;
+            }
+        }
+        return r;
+    }
+
+}
--- src/com/mkluo/dataalgo/test/Test.java
+++ src/com/mkluo/dataalgo/test/Test.java
@@ -9,16 +9,7 @@ package com.mkluo.dataalgo.test;
 public class Test {
 
     public static void main(String[] args) {
-        cal(1000);
+    int abc = 115;
+        System.out.println(abc%10);
     }
-
-    static int cal(int n) {
-        int sum =0;
-        for (int i = 1; i <= n; i++) {
-            sum = sum + i;
-        }
-        System.out.println(sum);
-        return sum;
-    }
-
 }
